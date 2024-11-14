dd671b4ec1cc42a07d2fac58e0b4c7ba641a2ed3
Bernardo Soriano <bersoriano@me.com>
2024-11-14 15:03:59 -0600

    Updates

---
+++ static/js/247.346f5e6d.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 247.346f5e6d.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkrenavest_mobile=self.webpackChunkrenavest_mobile||[]).push([[247],{247:(e,t,n)=>{n.r(t),n.d(t,{startStatusTap:()=>a});var s=n(146),r=n(721),o=n(384);const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,s.gv)((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const i=(0,r.a)(a);i&&new Promise((e=>(0,o.c)(i,e))).then((()=>{(0,s.bN)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,r.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=247.346f5e6d.chunk.js.map

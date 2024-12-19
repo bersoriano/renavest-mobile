0b08f1c575e86f1c3fd307da9904cf0ae1e1796d
Bernardo Soriano <bersoriano@me.com>
2024-12-18 23:05:11 -0600

    updated alert before booking

--- src/features/Profile/RequestMeeting.tsx
+++ src/features/Profile/RequestMeeting.tsx
@@ -7,9 +7,9 @@ import {
   IonContent,
   IonToolbar,
   IonTitle,
-  IonPage,
   IonItem,
   IonInput,
+  IonAlert
 } from '@ionic/react';
 import { OverlayEventDetail } from '@ionic/core/components';
 
@@ -22,29 +22,37 @@ function RequestMeeting({ meetingURL }: RequestMeetingProps) {
   const userName = useRef<HTMLIonInputElement>(null);
   const userEmail = useRef<HTMLIonInputElement>(null);
   const helpTopic = useRef<HTMLIonInputElement>(null);
-
-  const [message, setMessage] = useState(
-    'This modal RequestMeeting uses triggers to automatically open a modal when the button is clicked.'
-  );
+  const [isOpen, setIsOpen] = useState(false);
+  const [canDismiss, setCanDismiss] = useState(false);
 
   function confirm() {
     modal.current?.dismiss(userName.current?.value, 'confirm');
     //window.open(meetingURL, '_blank');
-    window.location.href = meetingURL;
+    //window.location.href = meetingURL;
+    setIsOpen(true);
+    setCanDismiss(false)
+  }
+
+  function cancel() {
+    setCanDismiss(true)
+    modal.current?.dismiss()
   }
 
   function onWillDismiss(ev: CustomEvent<OverlayEventDetail>) {
     if (ev.detail.role === 'confirm') {
-      setMessage(`Hello, ${ev.detail.data}!`);
     }
   }
 
   return (
-    <IonModal ref={modal} trigger="open-modal" onWillDismiss={(ev) => onWillDismiss(ev)}>
+    <IonModal 
+      ref={modal} 
+      trigger="open-modal" 
+      canDismiss={canDismiss} 
+      onWillDismiss={(ev) => onWillDismiss(ev)}>
         <IonHeader>
         <IonToolbar>
             <IonButtons slot="start">
-            <IonButton onClick={() => modal.current?.dismiss()}>Cancel</IonButton>
+            <IonButton onClick={() => cancel()}>Cancel</IonButton>
             </IonButtons>
             <IonTitle>Welcome</IonTitle>
             <IonButtons slot="end">
@@ -55,33 +63,46 @@ function RequestMeeting({ meetingURL }: RequestMeetingProps) {
         </IonToolbar>
         </IonHeader>
         <IonContent className="ion-padding">
-        <IonItem style={{ margin: '2rem 0' }}>
-            <IonInput
-            label="Enter your name"
-            labelPlacement="stacked"
-            ref={userName}
-            type="text"
-            placeholder="Your name"
-            />         
-        </IonItem>
-        <IonItem style={{ margin: '2rem 0' }}>
-            <IonInput
-            label="Enter your email"
-            labelPlacement="stacked"
-            ref={userEmail}
-            type="email"
-            placeholder="Your Email"
-            />         
-        </IonItem>
-        <IonItem style={{ margin: '2rem 0' }}>
-            <IonInput
-            label="What kind of help are you looking for?"
-            labelPlacement="stacked"
-            ref={helpTopic}
-            type="text"
-            placeholder="Write in a few words."
-            />         
-        </IonItem>        
+          <IonAlert
+            isOpen={isOpen}
+            header="Don't forget to Come Back!"
+            message="You are about to book an appointment through an external site to Renavest. Come back to Renavest by taping the back buttton."
+            buttons={[{
+              text: 'Continue',
+              handler: () => {
+                window.location.href = meetingURL;
+                console.log('Continue button clicked - redirecting to external booking site');
+              }
+            }]}
+            onDidDismiss={() => setIsOpen(false)}>
+          </IonAlert>          
+          <IonItem style={{ margin: '2rem 0' }}>
+              <IonInput
+              label="Enter your name"
+              labelPlacement="stacked"
+              ref={userName}
+              type="text"
+              placeholder="Your name"
+              />         
+          </IonItem>
+          <IonItem style={{ margin: '2rem 0' }}>
+              <IonInput
+              label="Enter your email"
+              labelPlacement="stacked"
+              ref={userEmail}
+              type="email"
+              placeholder="Your Email"
+              />         
+          </IonItem>
+          <IonItem style={{ margin: '2rem 0' }}>
+              <IonInput
+              label="What kind of help are you looking for?"
+              labelPlacement="stacked"
+              ref={helpTopic}
+              type="text"
+              placeholder="Write in a few words."
+              />         
+          </IonItem>        
         </IonContent>
     </IonModal>
   );
